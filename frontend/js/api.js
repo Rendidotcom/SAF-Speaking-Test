50fc4f222261a14c09b4f915a4647e11018d5882
Rendidotcom <holis112@gmail.com>
2026-07-29 12:09:05 +0000

    Fix api.js

--- frontend/js/api.js
+++ frontend/js/api.js
@@ -10,49 +10,63 @@ async function callAPI(action, data = {}) {
 
     try {
 
+        const response = await fetch(CONFIG.API_URL, {
+
+            method: "POST",
+
+            headers: {
+                "Content-Type": "application/json"
+            },
+
+            body: JSON.stringify({
+                action: action,
+                data: data
+            })
+
+        });
+
         const text = await response.text();
 
-console.log("STATUS :", response.status);
-console.log("RAW RESPONSE :", text);
+        console.log("STATUS :", response.status);
+        console.log("RAW RESPONSE :", text);
 
-try{
-    return JSON.parse(text);
-}catch(err){
-    return {
-        success:false,
-        message:"Invalid JSON",
-        raw:text
-    };
+        try {
 
-        const result = await response.json();
+            const result = JSON.parse(text);
 
-        if (CONFIG.DEBUG) {
+            if (CONFIG.DEBUG) {
 
-            console.log("=================================");
-            console.log("API REQUEST");
-            console.log(action);
-            console.log(data);
+                console.log("=================================");
+                console.log("API REQUEST");
+                console.log(action);
+                console.log(data);
 
-            console.log("API RESPONSE");
-            console.log(result);
-            console.log("=================================");
+                console.log("API RESPONSE");
+                console.log(result);
+                console.log("=================================");
+
+            }
+
+            return result;
+
+        } catch (err) {
+
+            return {
+                success: false,
+                message: "Invalid JSON Response",
+                raw: text
+            };
 
         }
-
-        return result;
 
     } catch (err) {
 
         console.error(err);
 
         return {
-
             success: false,
-
             message: "Cannot connect to server.",
-
             error: err.message
-
         };
 
     }
@@ -68,13 +82,9 @@ try{
 async function apiLogin(username, password, role) {
 
     return await callAPI("login", {
-
         username,
-
         password,
-
         role
-
     });
 
 }
